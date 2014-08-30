a280568a644cfd0123eab587504a2105cbbab250
Mathieu Patureau <mathieu.patureau@gmail.com>
2014-08-30 02:52:06 +0100

    update  lion cost

--- creatures/fleecemane_lion.rb
+++ creatures/fleecemane_lion.rb
@@ -7,7 +7,7 @@ class FleecemaneLion < Creature
     @strength = 3
     @toughness = 3
     @type = "Creature"
-    @cost = 2 # GW
+    @cost = 4 # GW
     @description =  "{3}{Green}{White}: Monstrosity 1. (If this creature isn't monstrous, put a +1/+1 counter on it and it becomes monstrous.) As long as Fleecemane Lion is monstrous, it has hexproof and indestructible."
     @img = "http://fc01.deviantart.net/fs71/f/2010/277/a/e/fantasy_lion_by_liontooth-d302q4s.jpg"
     @mtg_id = 373562
