b5738a468cc58cefd7b0c31a54a4af2e3fb8190e
Mathieu Patureau <mathieu.patureau@gmail.com>
2014-08-30 02:54:05 +0100

    Update elf.rb

--- creatures/elf.rb
+++ creatures/elf.rb
@@ -6,7 +6,7 @@ class Elf < Creature
     @strength = 1
     @toughness = 1
     @type = "Elf"
-    @cost = 1
+    @cost = 2
     @img = "https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcQTt6OYHNJ99yDLLkpmgc1Nn_S0-fVBc7qTVK985y8KfT4RoIqD"
     add_ability(HealAbility.new)
   end
