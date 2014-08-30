ae6aa407e08de548ca38dc1e4a70fb8dbe33e082
eltados <mathieu.patureau@gmail.com>
2014-08-30 03:56:31 +0100

    Re enable ajax requests

--- public/master.js
+++ public/master.js
@@ -5,20 +5,21 @@ var keymap = {
 };
 
 
-// $(document ).on( "click", "a.ajax" , function() {
-//   url = $( this ).attr("href") ;
-//   // alert("ajax");
-//   $.ajax({
-//     url: url,
-//     success: function( data ) {
-//       $('body').html(data);
-//     },
-//     error: function( data ) {
-//       $('body').html(data);
-//     }
-//   });
-//   return false;
-// });
+$(document ).on( "click", "a.ajax" , function() {
+  url = $( this ).attr("href") ;
+  // alert("ajax");
+  $.ajax({
+    url: url,
+    success: function( data ) {
+      $('body').html(data);
+    },
+    error: function( data ) {
+      $('body').html(data);
+    }
+  });
+  return false;
+});
+
 if(window.location.pathname  == "/game"){
 
 $( document ).keypress(function( event ) {
