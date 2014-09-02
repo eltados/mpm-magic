2be653414979518f9d7dde46c38c1eca83093ef3
Mathieu Patureau <mathieu.patureau@gmail.com>
2014-09-03 00:09:50 +0100

    Update dragon.rb

--- lib/cards/creatures/dragon.rb
+++ lib/cards/creatures/dragon.rb
@@ -6,7 +6,7 @@ class Dragon < Creature
     @strength = 7
     @toughness = 4
     @type = "Dragon"
-    @cost = 5
+    @cost = 6
     @img = "http://www.literarilycreative.com/wp-content/uploads/2013/03/The_green_dragon_by_Amisgaudi.jpg"
     add_abilities [ Flying ]
   end
