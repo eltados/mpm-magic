52693b3f93e93c9912282564ea3d4d2e7adf70dd
eltados <mathieu.patureau@gmail.com>
2014-10-08 01:19:20 +0100

    Evemn more ui tweaks

--- lib/actions/with_target/action_with_target.rb
+++ lib/actions/with_target/action_with_target.rb
@@ -20,9 +20,9 @@ class ActionWithTarget <Action
   def execute_with_target!(target)
     world.log Log.new(description: "#{card.name} #{self.name.downcase}s #{target.name}" , card: card, target: target , action:self)
     card.flags[:target] = [] if card.flags[:target] == nil
-    card.flags[:target] << {action:self , target:target , description: "#{card.name} #{self.name.downcase}s #{target.name}"}
+    card.flags[:target] << {action:self , target:target , description: "#{card.name} #{self.name.downcase}ed on #{target.name}"}
     target.flags[:target_by] = [] if target.flags[:target_by] == nil
-    target.flags[:target_by] << {action:self , target:card , description: "#{card.name} #{self.name.downcase}s #{target.name}"}
+    target.flags[:target_by] << {action:self , target:card , description: "#{card.name} #{self.name.downcase}ed on #{target.name}"}
   end
 
 
